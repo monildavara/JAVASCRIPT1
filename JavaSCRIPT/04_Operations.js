0d2a1711cd060206a32574083382cecb5ea84a33
Monil <297780146+monildavara@users.noreply.github.com>
2026-08-21 16:32:06 +0530

    Operations

---
+++ JavaSCRIPT/04_Operations.js
@@ -0,0 +1,38 @@
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello ";
+let str2 = "Monil"
+
+let str3 = (str1 + str2);
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(((1 + 2) * 5) % 1 );
+// console.log(+true);
+// console.log(+"");
+
+let Num1, Num2, Num3
+
+Num1 = Num2 = Num3 = 2 + 2 ;
+let GameCounter = 100;
+//GameCounter++;
+++GameCounter;
+console.log(GameCounter);
+
+
+
+
+
