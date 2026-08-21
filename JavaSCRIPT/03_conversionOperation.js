5c8cf25e23f02eb55970242a3d2ab6a8b30b6385
Monil <297780146+monildavara@users.noreply.github.com>
2026-08-21 16:01:18 +0530

    conversion

---
+++ JavaSCRIPT/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let Score = true;
+
+// console.log(typeof Score);
+// console.log(typeof (Score));
+
+let valueInNumber = Number(Score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+// " 33 " ==> 33
+// "33abc" ==> NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "MoNIL";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+//"MONIL " => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
