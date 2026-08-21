9ccca3bee0fe248952b2eca50f57003251990aa0
Monil <297780146+monildavara@users.noreply.github.com>
2026-08-21 17:33:42 +0530

    Data_type_summary

---
+++ Data_type/DataTypeSummary.js
@@ -0,0 +1,44 @@
+// primitive data type
+/*  String
+    Number 
+    Null
+    Boolearn
+    Undefined
+    Symbol
+    BigInt
+*/
+
+// NOTE :-JavaScript is a high-level, multi-paradigm, dynamically typed
+
+// Reference types (Non Primitive)
+// Object
+// Array
+// Functions
+
+const Score = 100;
+const ScoreValue = 100.3;
+const isLoggedIn = false;
+const outSideTemp = null;
+let UserEmail ; 
+
+const Id = Symbol('123');
+const AnotherId = Symbol('123');
+
+console.log(Id === AnotherId);
+
+// const BigNumber = 1234567890986543215647382910n
+
+const heros = ["Bheem", "Doremon", "Hathodi", "Motu-Patlu"];
+let MyObj = {
+    name : "Monil",
+    age : 22,
+};
+
+const MyFunction = function(){
+    console.log("HELLOW WORLD");
+}
+
+console.log(typeof AnotherId);
+
+
+
