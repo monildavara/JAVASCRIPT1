cd34c96090a07e0e2bd9fe2c76ec9257daffb7cd
Monil <297780146+monildavara@users.noreply.github.com>
2026-08-21 14:57:13 +0530

    fille loctin change

--- JavaS.js/demo.js
+++ JavaS.js/demo.js
@@ -6,11 +6,11 @@ Account_City = " Surat ";
 let Account_Stat ;
 
 // Account_Id = 2000
-// Account_Email = "monildavra@gmail.com";
-// Account_Password = "0000";
-// Account_City = " Surat, gujrat ";
+Account_Email = "monildavra@gmail.com";
+Account_Password = "0000";
+Account_City = " Surat, gujrat ";
 
-/* Prefer not use Var 
+/* Prefer not use Var
 because of issue in Block scope and Functional Scope
  */
 
