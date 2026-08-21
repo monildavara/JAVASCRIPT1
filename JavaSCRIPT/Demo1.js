3a6154257fd5a8bd579fc296461f83566d206a0d
Monil <297780146+monildavara@users.noreply.github.com>
2026-08-21 15:28:29 +0530

    Data type

--- JavaSCRIPT/Demo1.js
+++ JavaSCRIPT/Demo1.js
@@ -4,3 +4,23 @@
 
 console.log(3 + 3);
 console.log("DAVRA MONIL");
+
+// data type
+let Name = "MONIL";
+let age = 18;
+let isLoggedIn = false;
+let state ;
+
+
+// number => 2 to power 53
+//bigint
+// String ==> ""
+// boolean ==> true/ false
+//null => standalone value
+// undefined =>
+// Symbol => unique number 
+
+//Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
