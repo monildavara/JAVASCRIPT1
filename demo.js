ed3fb4db6a5b02588efe94a86c3b39d7cad893fe
Monil <297780146+monildavara@users.noreply.github.com>
2026-08-21 14:43:31 +0530

    variabal use

--- demo.js
+++ demo.js
@@ -3,6 +3,7 @@ const Account_Id = 100001;
 let Account_Email = "monil@gmail.com";
 var Account_Password = "1001";
 Account_City = " Surat ";
+let Account_Stat ;
 
 // Account_Id = 2000
 Account_Email = "monildavra@gmail.com";
@@ -10,9 +11,8 @@ Account_Password = "0000";
 Account_City = " Surat, gujrat ";
 
 console.log(Account_Id); 
-console.log(Account_Email);
-console.log(Account_Password);
-console.log(Account_City);
+
+console.table([Account_Id, Account_Email, Account_Password, Account_City, Account_Stat]);
 
 
 
