afa864915e4011ab35ef3a19c638e45045bf1f73
Monil <297780146+monildavara@users.noreply.github.com>
2026-08-21 14:30:57 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,18 @@
+console.log("MONIL");
+const Account_Id = 100001;
+let Account_Email = "monil@gmail.com";
+var Account_Password = "1001";
+Account_City = " Surat ";
+
+// Account_Id = 2000
+Account_Email = "monildavra@gmail.com";
+Account_Password = "0000";
+Account_City = " Surat, gujrat ";
+
+console.log(Account_Id); 
+console.log(Account_Email);
+console.log(Account_Password);
+console.log(Account_City);
+
+
+
