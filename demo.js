7ed1d9e8206e3294aa3c5352e66dbf2c33c26b8a
Monil <297780146+monildavara@users.noreply.github.com>
2026-08-21 14:54:35 +0530

    comment

--- demo.js
+++ demo.js
@@ -10,6 +10,10 @@ let Account_Stat ;
 // Account_Password = "0000";
 // Account_City = " Surat, gujrat ";
 
+/* Prefer not use Var 
+because of issue in Block scope and Functional Scope
+ */
+
 console.log(Account_Id);
 
 console.table([Account_Id, Account_Email, Account_Password, Account_City, Account_Stat]);
