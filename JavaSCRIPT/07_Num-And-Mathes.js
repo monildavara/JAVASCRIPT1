cd82d03439507bceeed5162f11f3e9159962f22f
Monil <297780146+monildavara@users.noreply.github.com>
2026-08-22 12:43:26 +0530

    NumANDMath

---
+++ JavaSCRIPT/07_Num-And-Mathes.js
@@ -0,0 +1,33 @@
+const score = 400;
+//console.log(score);
+
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const OtherNumber = 123.8933;
+// console.log(OtherNumber.toPrecision(3));
+
+const Hanred = 10000000000;
+// console.log(Hanred.toLocaleString('en-IN'));
+
+// ============================" MATHES "====================================
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4,7,9,11,3));
+
+// console.log(Math.max(4,7,9,11,3));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
